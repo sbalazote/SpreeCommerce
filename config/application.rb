43f281e4469468d3538f3e2fbaeaeda54ddcb444
luchopal <lucho005>
2017-02-08 08:13:36 -0300

    Revierto cambio en idioma

--- config/application.rb
+++ config/application.rb
@@ -9,18 +9,6 @@ Bundler.require(*Rails.groups)
 module SpreeCommerce
   class Application < Rails::Application
     
-    config.to_prepare do
-      # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
     config.i18n.default_locale = :'es-AR'
     config.i18n.locale = 'es-AR'
 
